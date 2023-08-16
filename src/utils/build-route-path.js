a075ad88058083eda4009ee3b8f41eec945de0bd
Antônio Thiago Neto <atn.coding@gmail.com>
2023-08-15 23:37:31 -0300

    Update build-route-path.js

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -3,5 +3,11 @@
 export function buildRoutePath(path) {
   const routeParamentersRegex = /:([a-zA-Z]+)/g;
 
-  console.log(Array.from(path.matchAll(routeParamentersRegex)));
+  const pathWithParams=  path.replaceAll(routeParamentersRegex, ' ([a-z0-9\-_]+)')
+
+  console.log(pathWithParams)
+
+  const test = /\/users\/([a-z0-9-_]+)/
+
+  const pathRegex  = new RegExp ('^${pathWithParams}')
 }
